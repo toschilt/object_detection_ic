4445968326e4caa23a1f2e5f7784ca8e5dfb1176
Lucas Toschi <lt.toschi@gmail.com>
2021-06-01 00:58:16 -0300

    Debugging

--- data_generator/object_detection_2d_data_generator.py
+++ data_generator/object_detection_2d_data_generator.py
@@ -1139,6 +1139,8 @@ class DataGenerator:
             print('len(batch_X) = ', len(batch_X))
             for i in range(0, len(batch_X)):
                 print('len(batch_X[', i, ']) = ', len(batch_X[i]))
+                for j in range(0, len(batch_X[i])):
+                    print('len(batch_X[', i, '][', j, ']) = 'len(batch_X[i][j]))
 
             #batch_X = np.array(list(itertools.zip_longest(*batch_X, fillvalue=0))).T
             if (batch_X.size == 0):
